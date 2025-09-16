4dccf6ddf3342ec0a7ae1741f5209c8a92669951
Krishna Kumar <kkbhagat20@gmail.com>
2025-09-17 00:52:12 +0530

    add updateOrderStatus endpoint to  allow delivery partners to update order status

--- src/controllers/order/order.js
+++ src/controllers/order/order.js
@@ -89,3 +89,46 @@ export const confirmOrder = async (req, reply) => {
 };
 
 
+
+
+export const updateOrderStatus = async (req, reply) => {
+  try {
+    const { orderId } = req.params;
+    
+    const { status, deliveryPersonLocation } = req.body;
+    
+    const { userId } = req.user;
+   
+    const deliveryPerson = await DeliveryPartner.findById(userId);
+
+    if (!deliveryPerson) {
+      return reply.status(404).send({ message: "Delivery Person not found" });
+    }
+
+    const order = await Order.findById(orderId);
+
+    if (!order) return reply.status(404).send({ message: "Order not found" });
+
+    if (["cancelled", "delivered"].includes(order.status)) {
+      return reply.status(400).send({ message: "Order cannot be updated" });
+    }
+
+    if (order.deliveryPartner.toString() !== userId) {
+      return reply.status(403).send({ message: "Unauthorized" });
+    }
+
+    order.status = status;
+    order.deliveryPersonLocation = deliveryPersonLocation;
+    await order.save();
+
+    req.server.io.to(orderId).emit("liveTrackingUpdates", order);
+
+    return reply.send(order);
+  } catch (error) {
+    return reply
+      .status(500)
+      .send({ message: "Failed to update order status", error });
+  }
+};
+
+
